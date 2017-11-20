693f7092b2a65a603389f0d77f5427a2e261383c
Jake Renzella <j@kerenzella.com>
2017-11-20 11:09:46 +1100

    NEW: Add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GIT_MACOS_REPO=https://github.com/dstil/maestro
+
+INSTALL_PATH"/opt/maestro"
+
+command -v git >/dev/null 2>&1 || { echo "Developer tools not installed, please run: \"xcode-select --install\" in the terminal and then rerun this script." >&2; exit;}
+
+git clone --depth 1 --branch master $GIT_MACOS_REPO "${INSTALL_PATH}"
